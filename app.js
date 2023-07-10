908b707e5788d443736f1a3b36262f9a967bb858
Tutialfred <106350481+Tutialfred@users.noreply.github.com>
2023-07-10 04:42:51 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const appDesarrollo = document.querySelector(".contratoInteligente")
+
+appDesarrollo.addEventListener("click",  function showAlert() {
+ Swal.fire({
+    title: 'App en desarrollo',
+    text: "La app se encuentra en desarrollo, arreglando bugs y detalles para ofrecerte la mejor experiencia posible!",
+    icon: 'info',
+    // showCancelButton: true,
+    confirmButtonColor: '#176efa',
+    confirmButtonText: 'Okay'
+    // cancelButtonColor: '',
+  })
+})
